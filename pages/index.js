e12436439b45764a1dbe3d972d6ec05a661eea08
Nicole Lima <nicolelima.eu@gmail.com>
2024-01-13 19:42:08 +0000

    modifica a mensagem da página inicial

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+// Componente React, uma função JS
+function Home() {
+    return <h1>Au revoir, Amandoca &lt;	333</h1>
+}
+
+// Sinalizando que a função Home será utilizada para renderizar a página
+// Exportando a função para fora do arquivo
+// Definindo o export PADRÃO, a partir dele, um componente vai chamar o outro, formando o layout e etc
+export default Home;
