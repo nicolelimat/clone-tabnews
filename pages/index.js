115f98cc2a6d791d04e94f122319ff866fd73e15
Nicole Lima <nicolelima.eu@gmail.com>
2024-01-26 08:29:02 +0000

    style: modifica a fonte utilizada no `index.js`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
 // Componente React, uma função JS
 function Home() {
-  return <h1>Au revoir, Amandoca &lt; 333</h1>;
+  const headingStyle = {
+    fontFamily: "sans-serif",
+    fontSize: "24px",
+  };
+
+  return <h1 style={headingStyle}>Something may be happening here :&#41;</h1>;
 }
 
 // Sinalizando que a função Home será utilizada para renderizar a página
