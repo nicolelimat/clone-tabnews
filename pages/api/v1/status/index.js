0698a9bd3e693c07a8824920791c06af3cc7c779
nicolelimat <nicolelima.eu@gmail.com>
2024-03-29 16:37:59 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,12 +2,45 @@ import database from "infra/database.js";
 
 // informar qual função desse arquivo tem a responsabilidade de retornar o status da página
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;"); // retorna o objeto inteiro, em rows
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  // query sem parâmetro
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  // query com parâmetros dinâmicos
+  const databaseOpenedConnectionsResult = await database.query(
+    // objeto para a filtragem do node-postgres contra SQL Injection funcionar
+    {
+      text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;", // count(*) conta quantas rows tem e ::int faz a conversão pra int direto no banco
+      values: [databaseName],
+    },
+  );
+
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
   // o next.js injeta esses dois objetos
   // o metodo .send("string") não assume qual tipo de teclado foi utilizado (charset)
   // .json({chave : "valor:"}) serializado
-  response.status(200).json({ chave: "mandando vibrações positivas" }); // fazendo o endpoint responder com algo
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue), // pq volta uma string
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  }); // fazendo o endpoint responder com algo
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,7 +1,19 @@
-test("GET to /api/v1/status should return 200", async () => { // arrow é por padrão síncrona
+test("GET to /api/v1/status should return 200", async () => {
+  // arrow é por padrão síncrona
   // fazendo uma requisição por dentro do teste
   // o fetch é um client http como qualquer outro, consegue fazer requests, receber responses e etc
   // necessário esperar que o fetch trace seu caminho
   const response = await fetch("http://localhost:3000/api/v1/status"); // função assíncrona para o await
   expect(response.status).toBe(200);
+
+  // certificando que o corpo da mensagem (response) está chegando
+  const responseBody = await response.json(); // função assíncrona, então é necessário o await
+  // expect(responseBody.updated_at).toBeDefined(); // toBeDefined só vê se a propriedade existe
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString(); // confirmando por parsing se o conteúdo é de fato uma data
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt); // contornando o "null" que retorna o início do timestamp
+
+  expect(responseBody.dependencies.database.version).toEqual("16.1");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
