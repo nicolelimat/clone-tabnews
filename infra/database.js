8d669aad3a657640ca36010a4c3ff0974a91f07a
nicolelimat <nicolelima.eu@gmail.com>
2024-04-03 17:36:06 -0300

    makes `database.js` `client.connect()` more robust to errors

--- infra/database.js
+++ infra/database.js
@@ -10,13 +10,21 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
   }); // comando síncrono que instantaneamente objeto paradão pronto pra ser conectado no banco
 
-  await client.connect(); // comando assíncrono, precisa esperar (await), e pra esperar precisa declarar a func como async
+  console.log("Credenciais do Postgres:", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
 
   try {
+    await client.connect(); // comando assíncrono, precisa esperar (await), e pra esperar precisa declarar a func como async
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
     console.error(error);
+    throw error; // lançando o erro, não só seu log
   } finally {
     await client.end();
   }
